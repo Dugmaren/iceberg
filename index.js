8d09c88b657e12311fe990b378c3692cf21387a4
home_dev <christopher.jt.anderson@gmail.com>
2018-03-09 12:35:52 -0800

    dev/prod environment variables ok

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there'});
-});
+    if(process.env.NODE_ENV === 'production')
+        res.send({ hi: 'PRODUCTION!',
+                    port: PORT});
+    else
+        res.send({ hi: 'DEV',
+        port: PORT});
+    });
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT);
